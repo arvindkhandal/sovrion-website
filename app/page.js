b5aa069e1fd31a6f42a52d4199d6690e61e213b0
Arvind Khandal <tarvindkhandal516@gmail.com>
2025-10-31 11:23:26 +0530

    fix: add homepage route

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Sovrion Website</h1>
+      <h1>Welcome to Sovrion!</h1>
+      <p>This is your homepage.</p>
     </main>
   );
 }
